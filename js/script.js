e66e14d63205c388d5910bf52d30b4d6c3de32f5
Corrado <corradoct@hotmail.it>
2020-06-25 17:54:16 +0200

    Ho creato la griglia dinamicamente con Handlebars

--- js/script.js
+++ js/script.js
@@ -4,8 +4,55 @@
 
 $(document).ready(
   function() {
+
+    // // Aggiungo l'evento click al quadrato
+    // $('.col').on('click',
+    //   function() {
+    //     // Assegno il quadrato cliccato ad una variabile
+    //     var clickedElement = this;
+    //     // Effettuo la chiamata ajax
+    //     $.ajax(
+    //       {
+    //         // Dichiaro l'endpoint della API e il metodo utilizzato
+    //     		url: 'https://flynn.boolean.careers/exercises/api/random/int',
+    //     		method: 'GET',
+    //         // In caso di successo della chiamata eseguo le operazioni
+    //     		success: function(data) {
+    //           // Assegno il risultato della chiamata ad una variabile
+    //     			var numero = data.response;
+    //           // Se il numero è minore o uguale a 5 il quadrato diventa giallo
+    //           if (numero <= 5) {
+    //             $(clickedElement).removeClass('green');
+    //             $(clickedElement).addClass('yellow');
+    //           } else {  // Altrimenti diventa verde
+    //             $(clickedElement).removeClass('yellow');
+    //             $(clickedElement).addClass('green');
+    //           }
+    //           // Inserisco il numero all'interno del quadrato
+    //           $(clickedElement).find('span').text(numero);
+    //     		},
+    //         // In caso di fallimento della chiamata faccio comparire un errore
+    //     		error: function() {
+    //     			alert("Si è verificato un errore");
+    //     		}
+    //     	}
+    //     );
+    //   }
+    // );
+
+
+    // Creo la griglia dinamicamente con Handlebars
+    var source = $('#griglia').html();
+    var template = Handlebars.compile(source);
+
+    var numeroQuadrati = 36;
+
+    for (var i = 0; i < numeroQuadrati; i++) {
+      $('.grid').append(template);
+    }
+
     // Aggiungo l'evento click al quadrato
-    $('.col').on('click',
+    $('.square').on('click',
       function() {
         // Assegno il quadrato cliccato ad una variabile
         var clickedElement = this;
